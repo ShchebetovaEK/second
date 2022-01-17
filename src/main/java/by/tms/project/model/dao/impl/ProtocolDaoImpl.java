204daad6830d9e61566aeb565ecbbcfcd7479f6a
ShchebetovaEK <polenka.92@list.ru>
2022-01-17 16:34:50 +0300

    Commit

--- src/main/java/by/tms/project/model/dao/impl/ProtocolDaoImpl.java
+++ src/main/java/by/tms/project/model/dao/impl/ProtocolDaoImpl.java
@@ -7,6 +7,23 @@ import by.tms.project.model.entity.Protocol;
 import java.util.List;
 
 public class ProtocolDaoImpl implements ProtocolDao {
+
+    private static ProtocolDaoImpl instance;
+
+    private ProtocolDaoImpl() {
+    }
+
+    /**
+     * Get instance
+     * @return instance.
+     */
+    public static ProtocolDao getInstance() {
+        if (instance == null) {
+            instance = new ProtocolDaoImpl();
+        }
+        return instance;
+    }
+
     @Override
     public List<Protocol> findAll() throws DaoException {
         return null;
--- src/main/java/by/tms/project/model/service/impl/ProtocolServiceImpl.java
+++ src/main/java/by/tms/project/model/service/impl/ProtocolServiceImpl.java
@@ -1,4 +1,33 @@
 package by.tms.project.model.service.impl;
 
-public class ProtocolServiceImpl {
+import by.tms.project.exception.ServiceException;
+import by.tms.project.model.dao.ProtocolDao;
+import by.tms.project.model.dao.impl.ProtocolDaoImpl;
+import by.tms.project.model.entity.Protocol;
+import by.tms.project.model.service.ProtocolService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+public class ProtocolServiceImpl implements ProtocolService {
+    private static final Logger logger = LogManager.getLogger();
+    private ProtocolDao protocolDao = ProtocolDaoImpl.getInstance();
+    private static ProtocolServiceImpl instance;
+
+    private ProtocolServiceImpl(){}
+
+    public static ProtocolServiceImpl getInstance( ){
+        if(instance == null){
+            instance = new ProtocolServiceImpl();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public List<Protocol> findAll() throws ServiceException {
+        return null;
+    }
 }
+
