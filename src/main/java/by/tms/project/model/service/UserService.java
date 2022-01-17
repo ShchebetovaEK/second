a3c4d515ef4ce7a98b18144811f2187426275b51
ShchebetovaEK <polenka.92@list.ru>
2022-01-17 16:30:01 +0300

    Commit

--- src/main/java/by/tms/project/model/service/UserService.java
+++ src/main/java/by/tms/project/model/service/UserService.java
@@ -4,6 +4,7 @@ import by.tms.project.exception.ServiceException;
 import by.tms.project.model.entity.User;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface UserService {
@@ -14,18 +15,19 @@ public interface UserService {
 
     Optional<User> findByLogin(String login) throws ServiceException;
 
+    Optional<User> findUserById(long id) throws ServiceException;
+
     boolean checkIfUserValidForRegistration(String login, String email) throws ServiceException;
 
-    boolean registrationUser(String login,String password,String email) throws ServiceException;
+    boolean registrationUser(String login, String password, String email) throws ServiceException;
 
-    void sendMessageRegistrationOnUserEmail (String login,String  email, String currentLocale) throws ServiceException;
+    void sendMessageRegistrationOnUserEmail(String login, String email, String currentLocale) throws ServiceException;
 
-    void setUserNewLogin (String login, String newLogin) throws ServiceException;
+    void setUserNewLogin(String login, String newLogin) throws ServiceException;
 
-    void setUserNewPassword(String login,String newPassword) throws ServiceException;
+    void setUserNewPassword(String login, String newPassword) throws ServiceException;
 
-
-
-
-
+    boolean registerNewUser(Map<String, String> userCheck) throws ServiceException;
 }
+
+
