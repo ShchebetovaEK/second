ada8b130812a63534dae3b22565182504f2b7fe3
ShchebetovaEK <polenka.92@list.ru>
2022-02-01 21:25:18 +0300

    Commit

--- src/test/java/by/tms/project/model/validator/impl/UserValidatorImplTest.java
+++ src/test/java/by/tms/project/model/validator/impl/UserValidatorImplTest.java
@@ -1,10 +1,41 @@
 package by.tms.project.model.validator.impl;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.DataProvider;
 
 class UserValidatorImplTest {
+
+    @DataProvider(name = "loginData")
+    public Object[][] createInvalidLogin() {
+        String[][] data = {{"1root"}, {"ro<o>t"}, {"main root"}, {"A"}, {"mainRootMainRootRoot1"}};
+        return data;
+    }
+
+    @DataProvider(name = "passwordData")
+    public Object[][] createInvalidPassword() {
+        String[][] data = {{"<script>alert(\"123\")</script>"}, {"33333333"}, {"9999999"}, {"333333DD"}, {"popovDGG"}
+                , {"-33333Dd"}, {"33333<>Dg"}};
+        return data;
+    }
+
+    @DataProvider(name = "nameData")
+    public Object[][] createInvalidName() {
+        String[][] data = {{""}, {" "}, {"I"}, {"DeN$s"}, {"Den<i>s"}, {"Den1s"}, {"LenaLenaLenaLenaLenaLenaLenaLenaLena"}};
+        return data;
+    }
+
+    @DataProvider(name = "emailData")
+    public Object[][] createInvalidEmail() {
+        String[][] data = {{"Lena-mail.ru"}, {"@mail.ru"}, {"Le<n>a@mail.ru"}, {"LenaLenaLenaLenaLenaLenaLenaLenaLenaLenaLenaLenaLena@mail.ru"}};
+        return data;
+    }
+
+    @DataProvider(name = "mobileNumberData")
+    public Object[][] createInvalidMobileNumber() {
+        String[][] data = {{"80447181933"}, {"80257181933"}, {"80297181933"}, {"+375337181933"}};
+        return data;
+    }
+
 
     @Test
     void isLoginValid() {
