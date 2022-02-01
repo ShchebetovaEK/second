51ba5a567af057a3dc785981a5e30aa3cb8fd076
ShchebetovaEK <polenka.92@list.ru>
2022-02-01 20:48:57 +0300

    Commit

---
+++ src/test/java/by/tms/project/model/validator/impl/UserValidatorImplTest.java
@@ -0,0 +1,40 @@
+package by.tms.project.model.validator.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserValidatorImplTest {
+
+    @Test
+    void isLoginValid() {
+    }
+
+    @Test
+    void isPasswordValid() {
+    }
+
+    @Test
+    void isFirstNameValid() {
+    }
+
+    @Test
+    void isLastNameValid() {
+    }
+
+    @Test
+    void isDataBirthdayValid() {
+    }
+
+    @Test
+    void isAddressValid() {
+    }
+
+    @Test
+    void isEmailValid() {
+    }
+
+    @Test
+    void isPhoneNumberValid() {
+    }
+}
