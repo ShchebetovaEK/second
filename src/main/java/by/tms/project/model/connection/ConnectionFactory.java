662d60c6cafcaaa16247371c908204e34648dacd
ShchebetovaEK <polenka.92@list.ru>
2022-02-04 00:45:52 +0300

    Commit

--- src/main/java/by/tms/project/model/connection/ConnectionFactory.java
+++ src/main/java/by/tms/project/model/connection/ConnectionFactory.java
@@ -12,34 +12,34 @@ import java.util.Properties;
 
 /**
  * @author ShchbetovaEK
- *
+ * <p>
  * class ConnectionFactory
  */
- class ConnectionFactory {
+class ConnectionFactory {
     private static final Logger logger = LogManager.getLogger();
     private static final Properties property;
-    private static  String DATABASE;
     private static final String DATABASE_URL_PROP = "url";
     private static final String PATH = "prop/db.properties";
+    private static String DATABASE;
 
     static {
         property = PropertyLoader.getProperty(PATH);
         try {
             Driver mysqlDriver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(mysqlDriver);
-        }catch (SQLException e){
-            logger.fatal("Driver don't have registration ",e);
-
+        } catch (SQLException e) {
+            logger.fatal("Driver don't have registration ", e);
         }
         DATABASE = property.getProperty(DATABASE_URL_PROP);
     }
+
     /**
      * Get connection.
      *
      * @return the connection
      * @throws SQLException the SQL exception
      */
-     static Connection getConnection() throws SQLException {
+    static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DATABASE, property);
     }
 
