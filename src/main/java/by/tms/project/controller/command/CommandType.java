7e3bd71bf5c8e5664173ad978600c8bbc15d0af2
ShchebetovaEK <polenka.92@list.ru>
2022-02-06 21:20:15 +0300

    Commit

--- src/main/java/by/tms/project/controller/command/CommandType.java
+++ src/main/java/by/tms/project/controller/command/CommandType.java
@@ -40,6 +40,11 @@ import java.util.List;
 
 import static by.tms.project.model.entity.Role.*;
 
+/**
+ * @author ShchbetovaEK
+ *
+ * enum CommandType
+ */
 public enum CommandType {
     /* move */
     GO_TO_ABOUT_PAGE_COMMAND(new GoToAboutCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
@@ -54,7 +59,7 @@ public enum CommandType {
     GO_TO_CHOOSE(new GoToChooseCommand(), List.of(PATIENT)),
 
     /* admin */
-    ADMIN_PAGE_COMMAND(new AdminTakeAllUsersCommand(), List.of(ADMIN, DOCTOR, PATIENT)),
+    ADMIN_PAGE_COMMAND(new AdminTakeAllUsersCommand(), List.of(ADMIN)),
     ADMIN_TAKE_ALL_DOCTORS_COMMAND(new AdminTakeAllDoctorsCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
     ADMIN_TAKE_DOCTORS_BY_CATEGORY_COMMAND(new AdminTakeDoctorByCategoryCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
     ADMIN_TAKE_DOCTORS_BY_EXPERIENCE_COMMAND(new AdminTakeDoctorByExperienceCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
@@ -64,12 +69,12 @@ public enum CommandType {
     ADMIN_REGISTER_ADMIN_COMMAND(new AdminRegisterAdminCommand(), List.of(ADMIN)),
     ADMIN_REGISTER_PROTOCOL_COMMAND(new AdminRegisterProtocolCommand(), List.of(ADMIN)),
 
-    ADMIN_TAKE_ALL_PATIENTS_COMMAND(new AdminTakeAllPatientsCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
-    ADMIN_TAKE_ALL_PATIENTS_BY_INSURANCE_COMMAND(new AdminTakePatientByInsuranceCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
-    ADMIN_TAKE_ALL_PATIENTS_BY_DISCOUNT_COMMAND(new AdminTakePatientByDiscountCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
-    ADMIN_TAKE_ALL_PATIENTS_BY_LOGIN_COMMAND(new AdminTakePatientByLoginCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
+    ADMIN_TAKE_ALL_PATIENTS_COMMAND(new AdminTakeAllPatientsCommand(), List.of(ADMIN, DOCTOR)),
+    ADMIN_TAKE_ALL_PATIENTS_BY_INSURANCE_COMMAND(new AdminTakePatientByInsuranceCommand(), List.of(ADMIN,DOCTOR)),
+    ADMIN_TAKE_ALL_PATIENTS_BY_DISCOUNT_COMMAND(new AdminTakePatientByDiscountCommand(), List.of(ADMIN,DOCTOR)),
+    ADMIN_TAKE_ALL_PATIENTS_BY_LOGIN_COMMAND(new AdminTakePatientByLoginCommand(), List.of(ADMIN,DOCTOR)),
 
-    ADMIN_TAKE_ALL_USERS_COMMAND(new AdminTakeAllUsersCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
+    ADMIN_TAKE_ALL_USERS_COMMAND(new AdminTakeAllUsersCommand(), List.of(ADMIN)),
 
     ADMIN_TAKE_ALL_PROTOCOLS_COMMAND(new AdminTakeAllProtocolsCommand(), List.of(ADMIN, DOCTOR)),
     ADMIN_TAKE_PROTOCOL_BY_DATA_COMMAND(new AdminTakeProtocolByDataCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
@@ -77,13 +82,13 @@ public enum CommandType {
     ADMIN_TAKE_PROTOCOL_BY_DOCTOR_COMMAND(new AdminTakeProtocolByDoctorCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
     ADMIN_TAKE_PROTOCOL_BY_PATIENT_COMMAND(new AdminTakeProtocolByPatientCommand(), List.of(ADMIN, DOCTOR)),
 
-    ADMIN_TAKE_USER_BY_EMAIL_COMMAND(new AdminTakeUserByEmailCommand(), List.of(ADMIN, DOCTOR, PATIENT)),
-    ADMIN_TAKE_USER_BY_FIRST_NAME_COMMAND(new AdminTakeUserByFirstNameCommand(), List.of(ADMIN, DOCTOR, PATIENT)),
-    ADMIN_TAKE_USER_BY_LAST_NAME_COMMAND(new AdminTakeUserByLastNameCommand(), List.of(ADMIN, DOCTOR, PATIENT)),
-    ADMIN_TAKE_USER_BY_ID_COMMAND(new AdminTakeUserByIdCommand(), List.of(ADMIN, DOCTOR, PATIENT)),
-    ADMIN_TAKE_USER_BY_LOGIN_COMMAND(new AdminTakeUserByLoginCommand(), List.of(ADMIN, DOCTOR, PATIENT)),
+    ADMIN_TAKE_USER_BY_EMAIL_COMMAND(new AdminTakeUserByEmailCommand(), List.of(ADMIN)),
+    ADMIN_TAKE_USER_BY_FIRST_NAME_COMMAND(new AdminTakeUserByFirstNameCommand(), List.of(ADMIN)),
+    ADMIN_TAKE_USER_BY_LAST_NAME_COMMAND(new AdminTakeUserByLastNameCommand(), List.of(ADMIN, DOCTOR )),
+    ADMIN_TAKE_USER_BY_ID_COMMAND(new AdminTakeUserByIdCommand(), List.of(ADMIN, DOCTOR)),
+    ADMIN_TAKE_USER_BY_LOGIN_COMMAND(new AdminTakeUserByLoginCommand(), List.of(ADMIN, DOCTOR)),
     ADMIN_TAKE_PROTOCOL_COST_COMMAND(new AdminTakeProtocolCostCommand(), List.of(ADMIN)),
-    MANAGER_PAGE_COMMAND(new AdminTakeAllUsersCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
+    MANAGER_PAGE_COMMAND(new AdminTakeAllUsersCommand(), List.of(ADMIN)),
 
     /*update*/
     UPDATE_USER_ADDRESS_COMMAND(new UpdateUserAddressCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
@@ -91,12 +96,12 @@ public enum CommandType {
     UPDATE_USER_FIRST_NAME_COMMAND(new UpdateUserFirstNameCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
     UPDATE_USER_LAST_NAME_COMMAND(new UpdateUserLastNameCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
     UPDATE_USER_PHONE_NUMBER_COMMAND(new UpdateUserPhoneNumberCommand(), List.of(ADMIN, PATIENT, DOCTOR)),
-    UPDATE_DOCTOR_CATEGORY_COMMAND(new UpdateDoctorCategoryCommand(), List.of(ADMIN, DOCTOR)),
-    UPDATE_DOCTOR_EXPERIENCE_COMMAND(new UpdateDoctorExperienceCommand(), List.of(ADMIN, DOCTOR)),
-    UPDATE_DOCTOR_SPECIALITY_COMMAND(new UpdateDoctorSpecialityCommand(), List.of(ADMIN, DOCTOR)),
-    UPDATE_PATIENT_INSURANCE_COMMAND(new UpdatePatientInsuranceCommand(), List.of(ADMIN, DOCTOR)),
-    UPDATE_PATIENT_DISCOUNT_COMMAND(new UpdatePatientDiscountCommand(), List.of(ADMIN, DOCTOR)),
-    UPDATE_PATIENT_MONEY_ACCOUNT_COMMAND(new UpdatePatientMoneyAccountCommand(), List.of(ADMIN, DOCTOR)),
+    UPDATE_DOCTOR_CATEGORY_COMMAND(new UpdateDoctorCategoryCommand(), List.of(ADMIN)),
+    UPDATE_DOCTOR_EXPERIENCE_COMMAND(new UpdateDoctorExperienceCommand(), List.of(ADMIN)),
+    UPDATE_DOCTOR_SPECIALITY_COMMAND(new UpdateDoctorSpecialityCommand(), List.of(ADMIN)),
+    UPDATE_PATIENT_INSURANCE_COMMAND(new UpdatePatientInsuranceCommand(), List.of(ADMIN)),
+    UPDATE_PATIENT_DISCOUNT_COMMAND(new UpdatePatientDiscountCommand(), List.of(ADMIN)),
+    UPDATE_PATIENT_MONEY_ACCOUNT_COMMAND(new UpdatePatientMoneyAccountCommand(), List.of(ADMIN)),
     ADMIN_UPDATE_PROTOCOL_COMMAND(new AdminUpdateProtocolCommand(), List.of(ADMIN)),
     ADMIN_UPDATE_PROTOCOL_APPLICATION_COMMAND(new AdminUpdateProtocolApplicationCommand(), List.of(ADMIN)),
     ADMIN_UPDATE_PROTOCOL_STATUS_COMMAND(new AdminUpdateProtocolStatusCommand(),List.of(ADMIN)),
@@ -107,7 +112,7 @@ public enum CommandType {
     ADMIN_DELETE_DOCTOR_COMMAND(new AdminDeletePatientCommand(), List.of(ADMIN)),
     ADMIN_DELETE_ADMIN_COMMAND(new AdminDeletePatientCommand(), List.of(ADMIN)),
 
-
+    /*only patient command*/
     PATIENT_CHOOSE_DOCTOR_COMMAND(new PatientChooseDoctorCommand(), List.of(PATIENT)),
     PATIENT_TAKE_PROTOCOL_COMMAND(new PatientTakeProtocolCommand(), List.of(PATIENT)),
     PATIENT_TAKE_ALL_DOCTOR_COMMAND(new PatientTakeAllDoctorsCommand(), List.of(PATIENT)),
